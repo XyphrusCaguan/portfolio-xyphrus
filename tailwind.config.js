cb4db28d74afc06514240880b3e4b3e01ab5c47e
XyphrusCaguan <caguanxyphrus@gmail.com>
2023-05-23 01:23:25 +0800

    update: Working tailwind set-up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,css,js,ts}"],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
     extend: {},
   },

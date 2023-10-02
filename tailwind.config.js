be700ea36598b4f4b4c65450874c9b925d50d3a2
XyphrusCaguan <caguanxyphrus@gmail.com>
2023-10-02 22:43:15 +0800

    media query on tailwind config js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    screens: {
+      'sm': '375px',
+      // => @media (min-width: 390px) { ... }
+      'md': '390px',
+      // => @media (min-width: 789px) { ... }
+      'lg': '789px',
+      // => @media (min-width: 1000px) { ... }
+    },
     extend: {},
   },
   plugins: [],
